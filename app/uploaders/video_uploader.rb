a699195c8699439893fee04d3b36955a73f001f0
ender554 <joshadams554@gmail.com>
2017-06-14 15:45:54 -0700

    lesson 24 video uploading succesful

--- app/uploaders/video_uploader.rb
+++ app/uploaders/video_uploader.rb
@@ -1,12 +1,12 @@
-class ImageUploader < CarrierWave::Uploader::Base
+class VideoUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  include CarrierWave::MiniMagick
+  # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  #storage :file
   storage :aws
+  # storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -45,5 +45,5 @@ class ImageUploader < CarrierWave::Uploader::Base
   # def filename
   #   "something.jpg" if original_filename
   # end
-  process resize_to_fill: [800,350]
+
 end
