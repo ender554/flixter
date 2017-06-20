5a41f48740da463cd4801055cae604700cf61fc9
ender554 <joshadams554@gmail.com>
2017-06-20 12:44:24 -0700

    error on viewing course

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,12 +1,27 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :user_enrolled!
+
   def show
   end
 
   private
 
+  def user_enrolled?
+    if current_user.enrolled_in?(current_lesson.section.course) != true
+      redirect_to :back, :alert => "You must be enrolled to view lesson!"
+    end
+  end
+
   helper_method :current_lesson
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end
+
+  helper_method :current_course
+  def current_course
+    @current_course ||= current_lesson.section.course
+  end
+
 
 end
